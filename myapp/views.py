c21d3c52cca8178844dc2d41b70dba022d7b1490
Suhail Singh Bains <bains.singh.suhail@gmail.com>
2022-06-15 14:49:54 -0400

    Lab 04 Complete

--- myapp/views.py
+++ myapp/views.py
@@ -2,42 +2,20 @@
 from django.http import HttpResponse
 from .models import Topic, Course, Student, Order
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    course_list = Course.objects.all().order_by('-price')[:5]
-    response = HttpResponse()
-    heading1 = '<p>' + 'List of Courses: ' + '</p>'
-    response.write(heading1)
-    for course in course_list:
-        para = '<p>'+ str(course.id) + '. ' + str(course) + ' Price: ' + str(course.price)  + ' | Course Availability: ' +\
-            '<a style="color:#008080"><b>'+ \
-               ('This Course is For Everyone!'
-                if (course.for_everyone == True)
-                else
-                'This Course is Not For Everyone!') +\
-            '</b></a></p>'
-        response.write(para)
-    return response
+    top_list = Topic.objects.all().order_by('id')[:10]
+    return render(request, 'myapp/index0.html', {'top_list': top_list})
 
 def about(request):
-    response = HttpResponse()
-    heading1 = '<p><a style="color:#008080"><b>' + "This is an E-learning Website!Search our Topics to find all available Courses." + '</b></a></p>'
-    response.write(heading1)
-    return response
+    return render(request, 'myapp/about0.html')
+
 
 def detail(request, top_no):
-    response = HttpResponse()
     course = Course.objects.filter(topic__id__contains = top_no)
     if(len(course) >0):
-        para = '<p>The topic found is: ' + str(course.first().topic.category) + '' + '</p>'
-        response.write(para)
-        response.write('</br><p>The Course List is as follows - </p>')
-        response.write('<ul>')
-        for item in course:
-            list = '<li>' + str(item.name) + '</li>'
-            response.write(list)
-        response.write('</ul>')
-        return response
+        return render(request, 'myapp/detail0.html', {'course': course})
     else:
         return get_object_or_404(Course, topic_id=top_no)
