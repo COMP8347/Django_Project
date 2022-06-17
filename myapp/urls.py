0bf58e26d1e03eccae6b784a53f4fd375e69deb8
Suhail Singh Bains <bains.singh.suhail@gmail.com>
2022-06-17 18:42:32 -0400

    Lab 06 Done

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,6 @@ from myapp import views
 app_name = 'myapp'
 urlpatterns = [
     path(r'', views.index, name='index'),
-    path(r'about', views.about),
-    path(r'<int:top_no>', views.detail)
+    path(r'about/', views.about),
+    path(r'<int:top_no>/', views.detail)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -7,15 +7,15 @@ from django.shortcuts import render
 
 def index(request):
     top_list = Topic.objects.all().order_by('id')[:10]
-    return render(request, 'myapp/index0.html', {'top_list': top_list})
+    return render(request, 'myapp/index.html', {'top_list': top_list})
 
 def about(request):
-    return render(request, 'myapp/about0.html')
+    return render(request, 'myapp/about.html')
 
 
 def detail(request, top_no):
     course = Course.objects.filter(topic__id__contains = top_no)
     if(len(course) >0):
-        return render(request, 'myapp/detail0.html', {'course': course})
+        return render(request, 'myapp/detail.html', {'course': course})
     else:
         return get_object_or_404(Course, topic_id=top_no)
