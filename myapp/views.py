0f7bf8342291a6dd8a6d638e2c20b4b394619a81
Suhail Singh Bains <bains.singh.suhail@gmail.com>
2022-06-17 18:46:12 -0400

    Fixes Formatting

--- myapp/views.py
+++ myapp/views.py
@@ -5,17 +5,19 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 # Create your views here.
 
+
 def index(request):
     top_list = Topic.objects.all().order_by('id')[:10]
     return render(request, 'myapp/index.html', {'top_list': top_list})
+
 
 def about(request):
     return render(request, 'myapp/about.html')
 
 
 def detail(request, top_no):
-    course = Course.objects.filter(topic__id__contains = top_no)
-    if(len(course) >0):
+    course = Course.objects.filter(topic__id__contains=top_no)
+    if(len(course) > 0):
         return render(request, 'myapp/detail.html', {'course': course})
     else:
         return get_object_or_404(Course, topic_id=top_no)
